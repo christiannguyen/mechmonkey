4c7da90f33a6ef918702f72da9add7a2df0874b9
Christian Nguyen <christiannguyen@christians-mbp.lan>
2021-02-10 13:42:28 -0800

    lolg

--- components/Accordion.js
+++ components/Accordion.js
@@ -10,6 +10,12 @@ const Accordion = (props) => {
         contentRef.current.children[0].scrollHeight,
         contentRef.current.scrollHeight
       );
+
+      console.log(
+        "what",
+        contentRef.current.children[0].scrollHeight,
+        contentRef.current.scrollHeight
+      );
       contentRef.current.style.maxHeight = active ? `${height}px` : "0px";
     }, 0);
   }, [contentRef, active]);
