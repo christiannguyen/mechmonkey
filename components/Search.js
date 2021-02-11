349ef673e6dfeae2c3515c1e5df8fb5a9387dccf
Christian Nguyen <christiannguyen@christians-mbp.lan>
2021-02-10 19:05:28 -0800

    input placeholder dark

--- components/Search.js
+++ components/Search.js
@@ -5,7 +5,7 @@ const Search = ({ query, setQuery, handler }) => {
     <>
       <form onSubmit={handler}>
         <input
-          className="py-2 pl-4 w-full border-2 border-gray-100 outline-none rounded-lg search-input dark:bg-gray-600 dark:text-gray-100 dark:border-gray-600"
+          className="py-2 pl-4 w-full border-2 border-gray-100 outline-none rounded-lg search-input dark:bg-gray-600 dark:text-gray-100 dark:border-gray-600 dark:placeholder-gray-300"
           type="text"
           placeholder="Search by title"
           value={query}
